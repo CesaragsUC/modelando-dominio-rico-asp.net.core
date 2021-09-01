53469889382b59af32afe22c725b34f0372c24d4
cesar augusto gadelha <cesar_ags@outlook.com>
2021-08-31 22:53:38 -0300

    criado entidades,valueObjects e testes

---
+++ NerdSore/src/NerdScore.Core/DomainObjects/Entity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdScore.Core.DomainObjects
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public override bool Equals(object obj)
+        {
+            var comparTo = obj as Entity;
+            if (ReferenceEquals(this, comparTo)) return true;
+            if (ReferenceEquals(null, comparTo)) return false;
+
+            return Id.Equals(comparTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null)) return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null)) return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} [Id={ Id}]";
+        }
+    }
+}
---
+++ NerdSore/tests/NerdStore.Catalogo.Domain.Tests/CategoriaTests.cs
@@ -0,0 +1,26 @@
+﻿using NerdScore.Catalogo.Domain;
+using NerdScore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NerdStore.Catalogo.Domain.Tests
+{
+    public class CategoriaTests
+    {
+
+        [Fact]
+
+        public void Validar_Categoria_DeveRetornarExecption()
+        {
+
+            var ex = Assert.Throws<DomainException>(() => new Categoria("Camisetas", 0));
+            Assert.Equal("O campo Codigo não pode ser 0", ex.Message);
+
+            ex = Assert.Throws<DomainException>(() => new Categoria(string.Empty, 1));
+            Assert.Equal("O campo Nome da categoria não pode estar vazio", ex.Message);
+
+        }
+    }
+}
